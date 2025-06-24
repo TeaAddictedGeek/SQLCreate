5b560522dec06061884f145825fe8291876b4472
Andy <andrea.berman@gmail.com>
2025-06-24 14:11:25 -0400

    upgrade to .NET 8.0, update data, create config

--- Program.cs
+++ Program.cs
@@ -1,33 +1,43 @@
 ﻿using System;
 using System.IO;
+using Microsoft.Extensions.Configuration;
 using OfficeOpenXml;
 
 namespace SQLCreate
 {
     class Program
     {
+        private static string _tableName = string.Empty;
+        private static string _filePath = string.Empty;
+
         static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json");
+
+            var config = configuration.Build();
+            Settings settings = (Settings)config.GetSection("Settings");
+            _filePath = settings.FilePath;
+            _tableName = settings.TableName;
             ReadExcel();
         }
 
         static void ReadExcel()
         {
-            ExcelPackage.LicenseContext = LicenseContext.Commercial;
             var dir = AppDomain.CurrentDomain.BaseDirectory;
-            using (var package = new ExcelPackage(new FileInfo(@"C:\Users\aberman\source\repos\SQLCreate\Data.xlsx")))
+            using (var package = new ExcelPackage(new FileInfo(_filePath)))
             {
                 var firstSheet = package.Workbook.Worksheets["Sheet1"];
                 var rowCount = firstSheet.Dimension.End.Row;
                 var colCount = firstSheet.Dimension.End.Column;
                 Console.WriteLine("Sheet 1 Data");
                 
-                for(int row = 2; row <= rowCount; row++) // start at two for the header
+                for(int row = 2; row <= rowCount; row++) // start at two to account for the header
                 {
-                    Console.WriteLine("Update public.\"Associate\"");
-                    Console.WriteLine($"set \"DateOfBirth\"='{firstSheet.Cells[row, 15].Text}'");
-                    Console.WriteLine($"where \"FirstName\"='{firstSheet.Cells[row, 6].Text}'");
-                    Console.WriteLine($"and \"LastName\"='{firstSheet.Cells[row, 7].Text}';");
+                    Console.WriteLine($"Update public.\"{_tableName}\"");
+                    Console.WriteLine($"set \"DateOfBirth\"='{firstSheet.Cells[row, 5].Text}'");
+                    Console.WriteLine($"where \"FirstName\"='{firstSheet.Cells[row, 1].Text}'");
+                    Console.WriteLine($"and \"LastName\"='{firstSheet.Cells[row, 2].Text}';");
                 }
                 Console.WriteLine("");
                 Console.ReadLine();
---
+++ Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLCreate
+{
+    public class Settings
+    {
+        public string TableName { get; set; }
+
+        public string FilePath { get; set; }
+    }
+}
